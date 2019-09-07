632d0e72b6de5acbb81fdee62815ac86ea9617ff
KienHT <hotrungkien1402@gmail.com>
2019-09-07 17:40:27 +0700

    update serverapp

--- ServerApp.py
+++ ServerApp.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import json
 from flask_cors import CORS
 import os
-
 app = Flask(__name__)
 CORS(app)
 baseURL = "./output"
@@ -17,11 +16,13 @@ def get_file_name(parentFolder, time):
 		file_time = filename.split(".")[0].split("_")[3]
 		if int(time_start) <= int(file_time) <= int(time_end):
 			resuslt.append(filename)
-	return  json.dumps(resuslt)
+	x = sorted(resuslt, reverse=True)
+	return  json.dumps(x)
 
 @app.route('/foldernames', methods = ['GET'])
 def get_folder_name():
 	foldernames = os.listdir(baseURL)
+	foldernames.sort(reverse=True)
 	return json.dumps(foldernames)
 
 @app.route('/filecontent/<parentFolder>/<filename>', methods = ['GET'])
@@ -35,26 +36,39 @@ def get_log_content(parentFolder, filename):
 	resources = input['resources']
 	index = 1;
 	for hc in hcOutputData:
-		print("check 1", hc["request_id"])
-		if hc['emp_id'] != "0":
-			if hc["emp_id"] in resuslt:
-				resuslt[hc["emp_id"]]['tasks'].append(get_assigned_task_by_request_id(hcOutputData, tasks, hc['request_id']))
+		if len(hc['request_id']) > 1:
+			if hc['emp_id'] != "0":
+				if hc["emp_id"] in resuslt:
+					print('check ...', hc['request_id'])
+					resuslt[hc["emp_id"]]['tasks'].append(get_assigned_task_by_request_id(hcOutputData, tasks, hc['request_id']))
+				else:
+					employee = get_employee_by_emp_id(resources,hc['emp_id'])
+					employee['tasks'] = [get_assigned_task_by_request_id(hcOutputData, tasks, hc['request_id'])]
+					resuslt[hc["emp_id"]] = employee
 			else:
-				employee = get_employee_by_emp_id(resources,hc['emp_id'])
+				employee = {}
+				employee['emp_id'] = ""
+				employee['emp_available'] = ""
+				employee['emp_type'] = ""
+				employee['emp_rank'] = ""
+				employee['emp_level'] = ""
+				employee['emp_status'] = ""
+				employee['emp_assigned'] = ""
 				employee['tasks'] = [get_assigned_task_by_request_id(hcOutputData, tasks, hc['request_id'])]
-				resuslt[hc["emp_id"]] = employee
-		else:
+				resuslt[str(index)] = employee
+				index += 1
+	for resource in resources:
+		if  resource['emp_id'] not in resuslt:
 			employee = {}
-			employee['emp_id'] = ""
-			employee['emp_available'] = ""
-			employee['emp_type'] = ""
-			employee['emp_rank'] = ""
-			employee['emp_level'] = ""
-			employee['emp_status'] = ""
-			employee['emp_assigned'] = ""
-			employee['tasks'] = [get_assigned_task_by_request_id(hcOutputData, tasks, hc['request_id'])]
-			resuslt[str(index)] = employee
-			index += 1
+			employee['emp_id'] = resource['emp_id']
+			employee['emp_available'] = resource['available']
+			employee['emp_type'] = resource['type']
+			employee['emp_rank'] = resource['rank']
+			employee['emp_level'] = resource['emp_level']
+			employee['emp_status'] = resource['emp_status']
+			employee['emp_assigned'] = resource['emp_assigned']
+			employee['tasks'] = []
+			resuslt[resource['emp_id']] = employee
 	rs = []
 	for x in resuslt.values():
 		rs.append(x)
@@ -78,7 +92,7 @@ def get_customer_level_by_request_id(tasks, request_id):
 	for task in tasks:
 		if int(task['request_id']) == int(request_id):
 			return task['vip2'], task['number_emp_needed'], task['sub_type_1']
-	return 0,0
+	return 0,0,0
 
 def get_assigned_task_by_request_id(hcOutputData, tasks, request_id):
 	rs = {}
@@ -91,6 +105,7 @@ def get_assigned_task_by_request_id(hcOutputData, tasks, request_id):
 			rs['task_type'] = hc['type']
 			rs['assigned'] = hc['assigned']
 			tup = get_customer_level_by_request_id(tasks, request_id)
+			print(tup)
 			rs['customer_level'] = tup[0]
 			rs['number_of_emp'] = tup[1]
 			rs['sub_type'] = tup[2]
@@ -110,4 +125,4 @@ def get_employee_by_emp_id(resources, emp_id):
 	return employee
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run()
